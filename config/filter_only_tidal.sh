01c85f061a926b97f081d31b78d88fc7c2da6665
mal2017 <>
2023-04-10 11:49:29 -0400

    fixed: include header in sample/subsample tables

--- config/filter_only_tidal.sh
+++ config/filter_only_tidal.sh
@@ -1,4 +1,8 @@
 # retains only strains analyzed in TIDAL
 
-grep -f ../../tetf_tidal/results/overlaps/tidal_strains.tsv sample_table.csv > sample_table.tidal.csv
-grep -f ../../tetf_tidal/results/overlaps/tidal_strains.tsv subsample_table.csv > subsample_table.tidal.csv    
+
+head -n 1 sample_table.csv > sample_table.tidal.csv
+head -n 1 sample_table.csv > subsample_table.tidal.csv
+
+grep -f ../../tetf_tidal/results/overlaps/tidal_strains.tsv sample_table.csv >> sample_table.tidal.csv
+grep -f ../../tetf_tidal/results/overlaps/tidal_strains.tsv subsample_table.csv >> subsample_table.tidal.csv    
