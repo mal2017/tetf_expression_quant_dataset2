2757edda067d990881ea0c8d367a912a52422113
mal2017 <>
2023-04-10 12:38:22 -0400

    bugfix: correct header in subsample table

--- config/filter_only_tidal.sh
+++ config/filter_only_tidal.sh
@@ -2,7 +2,7 @@
 
 
 head -n 1 sample_table.csv > sample_table.tidal.csv
-head -n 1 sample_table.csv > subsample_table.tidal.csv
+head -n 1 subsample_table.csv > subsample_table.tidal.csv
 
 grep -f ../../tetf_tidal/results/overlaps/tidal_strains.tsv sample_table.csv >> sample_table.tidal.csv
 grep -f ../../tetf_tidal/results/overlaps/tidal_strains.tsv subsample_table.csv >> subsample_table.tidal.csv    
